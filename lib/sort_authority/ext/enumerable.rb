d186e6e8c079975e7f9932da7d6c54e390ba6562
Nathan Fixler <nathan@fixler.org>
2013-12-19 08:34:59 -0800

    Guard against sorting nil values
    strnatcmp.c expects strings, but this can result in a very ugly error if
    the enumerable contains a nil. Let's raise an exception so that the
    expectation that all values be strings is clearer.

--- lib/sort_authority/ext/enumerable.rb
+++ lib/sort_authority/ext/enumerable.rb
@@ -1,17 +1,21 @@
 module Enumerable
   def natural_sort
+    SortAuthority.ensure_strings(self)
     sort {|a, b| SortAuthority.strnatcmp(a, b) }
   end
 
   def natural_sort!
+    SortAuthority.ensure_strings(self)
     sort! {|a, b| SortAuthority.strnatcmp(a, b) }
   end
 
   def natural_sort_by(&block)
+    SortAuthority.ensure_strings(self.map {|val| block.call(val) })
     sort {|a, b| SortAuthority.strnatcmp(block.call(a), block.call(b)) }
   end
 
   def natural_sort_by!(&block)
+    SortAuthority.ensure_strings(self.map {|val| block.call(val) })
     sort! {|a, b| SortAuthority.strnatcmp(block.call(a), block.call(b)) }
   end
 end
